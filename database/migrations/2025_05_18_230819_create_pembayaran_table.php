f23821370196bb118b2923bf5b570dcbf4386d32
aydiahcntptry <ayudiahcintaputry@gmail.com>
2025-05-19 06:29:42 +0700

    update mulu huhu

---
+++ database/migrations/2025_05_18_230819_create_pembayaran_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pembayaran', function (Blueprint $table) {
+            $table->id('payment_id');
+            $table->unsignedBigInteger('order_id');
+            $table->decimal('amount', 15, 2);
+            $table->enum('payment_status', ['tertunda', 'dibayar'])->default('tertunda');
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('order_id')->on('pesanan')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pembayaran');
+    }
+};
